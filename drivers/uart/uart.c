5a9dd2a20b30d5366b01047d589ed0747600e236
Prathveesh <prathveesh240301@gmail.com>
2026-07-07 01:03:54 +0530

    feat(uart): update UART driver interface

--- drivers/uart/uart.c
+++ drivers/uart/uart.c
@@ -3,69 +3,68 @@
  * @brief   UART Driver implementation.
  *
  * @details
- * This file implements the UART Driver public interface and contains all
- * private helper functions required to configure and operate the UART
- * peripheral.
+ * This file implements the UART Driver using direct register access through
+ * CMSIS. The driver provides blocking transmit and receive functionality
+ * for USART2 on the STM32F303 Discovery board.
  ******************************************************************************/
 
 /* Includes */
 
 #include "uart.h"
 
-// #include "stm32f303xc.h"
+#include "stm32f303xc.h"
 
 /* Private Macros */
 
-/* USART2 Peripheral */
-#define UART_INSTANCE USART2
+/* USART Instance */
+#define UART_INSTANCE (USART2)
 
-/* GPIO Configuration */
-#define UART_GPIO_PORT GPIOA
+/* GPIO Port */
+#define UART_GPIO_PORT (GPIOA)
+
+/* GPIO Pin Configuration */
 #define UART_TX_PIN (2U)
 #define UART_RX_PIN (3U)
+
+/* GPIO Alternate Function */
 #define UART_GPIO_AF (7U)
 
 /* UART Configuration */
 #define UART_BAUDRATE (115200U)
 
+/*
+ * NOTE:
+ *
+ * For Release v1.0 we assume the APB1 peripheral clock driving USART2
+ * is configured to 8 MHz.
+ *
+ * BRR = PCLK / BaudRate
+ *
+ * 8000000 / 115200 = 69 (0x45)
+ *
+ * This value will later be calculated automatically once the Clock Driver
+ * is introduced into the project.
+ */
+#define UART_BRR_VALUE (69U)
+
 /* Private Function Prototypes */
 
-static void UART_EnableClock(void);
+static void UART_EnablePeripheralClock(void);
+
 static void UART_ConfigureGPIO(void);
-static void UART_ConfigurePeripheral(void);
+
+static void UART_ConfigureUSART(void);
 
 static UART_Status_t UART_WriteByte(uint8_t data);
 
 /* Public Function Definitions */
 
 UART_Status_t UART_Init(void) {
-  /* Implementation */
+  UART_EnablePeripheralClock();
 
-  return UART_STATUS_OK;
-}
+  UART_ConfigureGPIO();
 
-UART_Status_t UART_Write(const uint8_t *buffer, uint32_t length) {
-  /* Implementation */
-
-  return UART_STATUS_OK;
-}
-
-UART_Status_t UART_ReadByte(uint8_t *data) {
-  /* Implementation */
-
-  return UART_STATUS_OK;
-}
-
-/* Private Function Definitions */
-
-static void UART_EnableClock(void) {}
-
-static void UART_ConfigureGPIO(void) {}
-
-static void UART_ConfigurePeripheral(void) {}
-
-static UART_Status_t UART_WriteByte(uint8_t data) {
-  (void)data;
+  UART_ConfigureUSART();
 
   return UART_STATUS_OK;
 }
